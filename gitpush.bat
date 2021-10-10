acaa1cc2121282c9f58c5a6a4265a468cb794882
RaviThejaReddy <61244449+RaviThejaReddy@users.noreply.github.com>
2021-10-10 13:32:26 +0530

    Add files via upload

---
+++ gitpush.bat
@@ -0,0 +1,6 @@
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+set timestamp=%mydate%_%mytime%
+git add .
+git commit -m "updating at %timestamp%"
+git push
