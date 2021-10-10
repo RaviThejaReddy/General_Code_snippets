be8216d306b44a1d8846c2edb4fe2e978642cf85
RaviThejaReddy <61244449+RaviThejaReddy@users.noreply.github.com>
2021-10-10 13:37:22 +0530

    Update dowload_video_from_links.py

--- dowload_video_from_links.py
+++ dowload_video_from_links.py
@@ -2,17 +2,9 @@ import requests
 import os
 import re
 
+url_list = ['url1','url2']
 
-for x in os.listdir('./'):
-    if '.' not in x:
-        for y in os.listdir('./'+ x):
-            if '.yaml' in y:
-                with open('./'+ x + '/'+ y) as yamlfile:
-                    urllist = [x for x in yamlfile.readlines() if 'https://' in x]
-                    for url in urllist:
-                        url_string = url[15:-1]
-                        file_name = re.sub('[^0-9a-zA-Z]+','_',url_string.split('/')[-1]).replace('_mp4','.mp4')
-                        print(file_name)
-                        r = requests.get(url_string, allow_redirects=True)
-                        open(file_name, 'wb').write(r.content)
-                        
+for url in url_list:
+    file_name = re.sub('[^0-9a-zA-Z]+','_',url.split('/')[-1])
+    r = requests.get(url_string, allow_redirects=True)
+    open(file_name, 'wb').write(r.content)
